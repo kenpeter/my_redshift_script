46f4a3f6f14637b72b88229921be02647de752c1
kenpeter <figo2476@hotmail.com>
2015-05-30 09:33:53 +1000

    get weather temp from json api

--- my_redshift.sh
+++ my_redshift.sh
@@ -7,19 +7,36 @@ require 'httparty'
 # http://stackoverflow.com/questions/18581792/ruby-on-rails-and-json-parser-from-url
 # http://stackoverflow.com/questions/23030344/how-can-i-get-latitude-and-longitude-from-json-web-service
 address = "melbourne victoria"
-url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&sensor=true"
-response = HTTParty.get(url)
+address_weather = address.gsub(' ', ',')
+geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&sensor=true"
+response = HTTParty.get(geo_url)
 json = JSON.parse(response.body)
+my_extra_temp = 5000
 
 lat = json['results'][0]['geometry']['location']['lat'].to_s
 lng = json['results'][0]['geometry']['location']['lng'].to_s
 
 p 'Current location: ' + address
-p 'API url: ' + url
+p 'API url: ' + geo_url
 p 'lat: ' + lat
 p 'lng: ' + lng
 
-redshift_cmd = "gtk-redshift -l " + lat + ":" + lng + " -t 5700:5300 -g 0.9 -o -m vidmode:screen=0"
+# Weather
+# http://openweathermap.org/current
+weather_url = "http://api.openweathermap.org/data/2.5/weather?q=" + address_weather
+response_weather = HTTParty.get(weather_url)
+json_weather = JSON.parse(response_weather.body)
+
+temp = json_weather['main']['temp'].round
+# I need more red color
+my_temp = (temp + my_extra_temp).to_s
+
+p 'Weather api: ' + weather_url
+p 'Temperature: ' + my_temp + ' kelvin'
+
+redshift_cmd = "gtk-redshift -l " + lat + ":" + lng + " -t " + my_temp + ":" + my_temp + " -g 0.9 -o -m vidmode:screen=0"
+
+# This is used to test
 #redshift_cmd = "gtk-redshift -l -37.9:144.7 -t 5700:5300 -g 1 -o -m vidmode:screen=0"
 
 p "Command is: " + redshift_cmd
